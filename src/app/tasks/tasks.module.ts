b946fcc18b299ea8e44aaec8ad0668aef945c4a9
AndresLince <lynxdarmatron@gmail.com>
2024-09-04 17:51:19 -0500

    Import task and tasks components

--- src/app/tasks/tasks.module.ts
+++ src/app/tasks/tasks.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TasksRoutingModule } from './tasks-routing.module';
+import { TasksComponent } from './tasks/tasks.component';
+import { TaskComponent } from './task/task.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [TasksComponent, TaskComponent],
   imports: [
     CommonModule,
     TasksRoutingModule
