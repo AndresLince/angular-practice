2363964a8414ed32bde4f8e4f4cd21db514ead26
AndresLince <lynxdarmatron@gmail.com>
2024-09-04 20:09:47 -0500

    Make task form component as standalone false

--- src/app/tasks/task-form/task-form.component.ts
+++ src/app/tasks/task-form/task-form.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-task-form',
-  standalone: true,
-  imports: [],
+  standalone: false,
   templateUrl: './task-form.component.html',
   styleUrl: './task-form.component.css'
 })
