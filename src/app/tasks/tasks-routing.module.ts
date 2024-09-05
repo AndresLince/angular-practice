c5392ac731b2e033b1e256baeb61affb9d3c96fd
AndresLince <lynxdarmatron@gmail.com>
2024-09-04 20:08:37 -0500

    Add taskformcomponent to tasks routes

--- src/app/tasks/tasks-routing.module.ts
+++ src/app/tasks/tasks-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TasksComponent } from './tasks/tasks.component';
+import { TaskFormComponent } from './task-form/task-form.component';
 
 const routes: Routes = [
   {
     path: '',
     component: TasksComponent
+  },
+  {
+    path: 'create',
+    component: TaskFormComponent
   },
 ];
 
