d14ae9bbc149b2f670c2689846fce3dac68a7da6
AndresLince <lynxdarmatron@gmail.com>
2024-09-14 13:39:20 -0500

    Add clients

--- src/app/clients/clients/clients.component.ts
+++ src/app/clients/clients/clients.component.ts
@@ -7,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrl: './clients.component.css'
 })
 export class ClientsComponent {
-
+  public clients = [
+    { name: 'Client 1', id: 1, imageAlt: 'Image of client 1' },
+    { name: 'Client 2', id: 2, imageAlt: 'Image of client 2' },
+    { name: 'Client 3', id: 3, imageAlt: 'Image of client 3' },
+    { name: 'Client 4', id: 4, imageAlt: 'Image of client 4' },
+    { name: 'Client 5', id: 5, imageAlt: 'Image of client 5' },
+  ];
 }
