9f532c9b106eb4bce66a035eccd1cec1ac18f826
AndresLince <lynxdarmatron@gmail.com>
2024-09-14 13:39:58 -0500

    Add declarations of client and clients components

--- src/app/clients/clients.module.ts
+++ src/app/clients/clients.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ClientsRoutingModule } from './clients-routing.module';
+import { ClientComponent } from './client/client.component';
+import { ClientsComponent } from './clients/clients.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ ClientComponent, ClientsComponent],
   imports: [
     CommonModule,
     ClientsRoutingModule
