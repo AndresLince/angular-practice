bc5e7c566b594b611b1ec3c0fb3e191faef3e366
AndresLince <lynxdarmatron@gmail.com>
2024-09-14 16:58:39 -0500

    Add clients fields

--- src/app/clients/clients/clients.component.ts
+++ src/app/clients/clients/clients.component.ts
@@ -8,10 +8,10 @@ import { Component } from '@angular/core';
 })
 export class ClientsComponent {
   public clients = [
-    { name: 'Client 1', id: 1, imageAlt: 'Image of client 1' },
-    { name: 'Client 2', id: 2, imageAlt: 'Image of client 2' },
-    { name: 'Client 3', id: 3, imageAlt: 'Image of client 3' },
-    { name: 'Client 4', id: 4, imageAlt: 'Image of client 4' },
-    { name: 'Client 5', id: 5, imageAlt: 'Image of client 5' },
+    { name: 'Client 1', id: 1, imageAlt: 'Image of client 1', position: 'CEO', twitterAlias: '@client1' },
+    { name: 'Client 2', id: 2, imageAlt: 'Image of client 2', position: 'Php developer', twitterAlias: '@client1' },
+    { name: 'Client 3', id: 3, imageAlt: 'Image of client 3', position: 'Web UI', twitterAlias: '@client1' },
+    { name: 'Client 4', id: 4, imageAlt: 'Image of client 4', position: 'Team lead', twitterAlias: '@client1' },
+    { name: 'Client 5', id: 5, imageAlt: 'Image of client 5', position: 'Java developer', twitterAlias: '@client1' },
   ];
 }
