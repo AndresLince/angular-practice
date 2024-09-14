396d457e88488ae39919f0209e54e48a295ca654
AndresLince <lynxdarmatron@gmail.com>
2024-09-14 13:40:19 -0500

    Add logic to client component

--- src/app/clients/client/client.component.ts
+++ src/app/clients/client/client.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-client',
-  standalone: true,
-  imports: [],
   templateUrl: './client.component.html',
   styleUrl: './client.component.css'
 })
 export class ClientComponent {
+  @Input() client: { name: string, id: number, imageAlt: string };
 
+  constructor() {
+    this.client = { name: '', id: 0, imageAlt: '' };
+  }
+
+  getClientImageRoute(clientId: number) {
+    return `https://randomuser.me/api/portraits/men/${clientId}.jpg`;
+  }
 }
