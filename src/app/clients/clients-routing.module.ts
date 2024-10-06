3774df631e20e2be797df03f0f0b5e9b8fbe6675
AndresLince <lynxdarmatron@gmail.com>
2024-10-06 18:07:26 -0500

    Add client-detail component to client routes

--- src/app/clients/clients-routing.module.ts
+++ src/app/clients/clients-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ClientsComponent } from './clients/clients.component';
+import { ClientDetailComponent } from './client-detail/client-detail.component';
 
 const routes: Routes = [
   {
     path: '',
     component: ClientsComponent
   },
+  {
+    path: 'detail/:id',
+    component: ClientDetailComponent
+  }
 ];
 
 @NgModule({
